3b35b25a8c6b1e42332de4e6d41de5cefb793e66
NapasIsSky <napas.kavalee@gmail.com>
2023-03-29 06:45:06 +0700

    change file structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
-import SpaceSkyParallax from "@/modules/parallax/components/SpaceSkyParallax";
 import React from "react";
+
+import { CloudLeftIcon, CloudRigthIcon, MoonIcon, SuturnIcon } from "@/components/icon";
+
+import Style from "@/styles/SpaceSkyParallax.module.css";
 
 export default function Home() {
   return (
     <React.Fragment>
       <section>
-        <SpaceSkyParallax />
+        <div className={Style.spaceSkyBg}>
+          <SuturnIcon />
+          <MoonIcon className={Style.moon} />
+          <CloudLeftIcon className={Style.cloudLeft} />
+          <CloudRigthIcon className={Style.cloudRight} />
+        </div>
       </section>
     </React.Fragment>
   );
